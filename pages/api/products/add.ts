62fe7b1d4b1716698cc548ac4d364aa6d78064c4
Ulugbekdev <zairovulugbek2020@outlook.com>
2022-05-19 21:30:11 +0500

    changed add.ts in products of api routes

--- pages/api/products/add.ts
+++ pages/api/products/add.ts
@@ -1,5 +1,4 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import type { Product } from '../../../lib/types';
 import sqlite3 from 'sqlite3';
 let db = new sqlite3.Database('./base/admin.db');
 
@@ -7,9 +6,9 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     db.run(
         'INSERT INTO products (name, price, status, img_src) VALUES (?, ?, ?, ?)',
         [req.body.name, req.body.price, req.body.status, req.body.imgSrc],
-        (err: any, row: any) => {
+        (err: any) => {
             if (err) return res.json({ message: 'Add product. Error status code 500', statusCode: 500 });
-            
+
             res.json({
                 message: 'Adding product',
                 statusCode: 200
